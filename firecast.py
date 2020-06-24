b8fcd12593354fb896e4456a0f84c8efa2822a62
Brian Xu <xu.brian.s@gmail.com>
2020-06-24 15:39:32 -0700

    Tune FireCast model to data constraints

--- firecast.py
+++ firecast.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 
 
 class FireCast(nn.Module):
-    def __init__(self, area):
+    def __init__(self, area=15):
         super(FireCast, self).__init__()
         # self.avg_pool1 = nn.AvgPool2d(kernel_size=2)
-        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=6, out_channels=32, kernel_size=3, stride=1),
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=6, out_channels=32, kernel_size=2, stride=1),
                                    nn.Sigmoid(),
                                    nn.MaxPool2d(kernel_size=2),
                                    nn.Dropout2d())
-        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1),
                                    nn.ReLU(),
                                    nn.MaxPool2d(kernel_size=2),
                                    nn.Dropout2d())
@@ -20,9 +20,9 @@ class FireCast(nn.Module):
         def conv2d_result(size, kernel_size, stride=1):
             return (size - kernel_size) // stride + 1
 
-        conv2d_size = conv2d_result(area, 3)
+        conv2d_size = conv2d_result(area, 2)
         conv2d_size = conv2d_result(conv2d_size, 2, 2)
-        conv2d_size = conv2d_result(conv2d_size, 3)
+        conv2d_size = conv2d_result(conv2d_size, 2)
         conv2d_size = conv2d_result(conv2d_size, 2, 2)
         linear_input_size = (conv2d_size ** 2) * 64
 
