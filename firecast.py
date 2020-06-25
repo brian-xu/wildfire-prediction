0ce842f178875fb7d99910c058dccc1b74b2ab3e
Brian Xu <xu.brian.s@gmail.com>
2020-06-24 17:36:55 -0700

    Update model architecture

--- firecast.py
+++ firecast.py
@@ -9,11 +9,11 @@ class FireCast(nn.Module):
         # self.avg_pool1 = nn.AvgPool2d(kernel_size=2)
         self.conv1 = nn.Sequential(nn.Conv2d(in_channels=6, out_channels=32, kernel_size=2, stride=1),
                                    nn.Sigmoid(),
-                                   nn.MaxPool2d(kernel_size=2),
+                                   nn.MaxPool2d(kernel_size=2, stride=2),
                                    nn.Dropout2d())
         self.conv2 = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1),
                                    nn.ReLU(),
-                                   nn.MaxPool2d(kernel_size=2),
+                                   nn.MaxPool2d(kernel_size=2, stride=2),
                                    nn.Dropout2d())
 
         # Calculates tensor size after a Conv2d
@@ -24,18 +24,20 @@ class FireCast(nn.Module):
         conv2d_size = conv2d_result(conv2d_size, 2, 2)
         conv2d_size = conv2d_result(conv2d_size, 2)
         conv2d_size = conv2d_result(conv2d_size, 2, 2)
-        linear_input_size = (conv2d_size ** 2) * 64
+        self.linear_input_size = (conv2d_size ** 2) * 64
 
         # classifier
-        self.dense1 = nn.Linear(linear_input_size, 128)
+        self.dense1 = nn.Linear(self.linear_input_size, 128)
         self.dense2 = nn.Linear(133, 1)
 
     def forward(self, terrain, weather):
         # terrain = self.avg_pool1(terrain)
         x1 = self.conv1(terrain)
         x1 = self.conv2(x1)
+        x1 = x1.view(-1, self.linear_input_size)
         x1 = self.dense1(x1)
-        x2 = weather
+        x1 = F.relu(x1)
+        x2 = torch.tensor(weather)
 
         x = torch.cat((x1, x2))
         x = self.dense2(x)
