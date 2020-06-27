04183029fce8037409ba1671d195bdb3602a5a05
Brian Xu <xu.brian.s@gmail.com>
2020-06-26 20:38:42 -0700

    Add data normalization and validation

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torchvision.transforms as transforms
 
 from firecast import FireCast
 from wildfire_data_loader import WildfireDataset
@@ -9,26 +10,29 @@ from wildfire_data_loader import WildfireDataset
 DATASET_PATH = './wildfire_data/uci_ml_hackathon_fire_dataset_2012-05-09_2013-01-01_10k_train_v2-002.hdf5'
 trainset = WildfireDataset(DATASET_PATH)
 
+norm = transforms.Normalize(trainset.mean, trainset.std)
+
 trainloader = torch.utils.data.DataLoader(trainset, shuffle=True)
 
 area = 15
 peri = (area - 1) // 2
 net = FireCast(area, terrain_features=trainset.terrain_features, weather_features=trainset.weather_features)
 
-lr = 0.0005
+lr = 0.005
 criterion = nn.BCELoss()
 optimizer = optim.RMSprop(net.parameters(), lr=lr)
 epochs = 10
+batches = 0
 
 for epoch in range(epochs):
     running_loss = 0.0
     for i, data in enumerate(trainloader):
+        data = norm(data[0]).reshape(data.shape)
         pad_width = ((0, 0), (0, 0), (peri, peri), (peri, peri))
         terrain_full = np.pad(data[:, :6], pad_width)
         weather_full = data[:, 6:-1]
         target = data[:, -1]
 
-        batches = 0
         output = np.zeros(target.shape)
         for x in range(target.shape[1]):
             for y in range(target.shape[2]):
@@ -40,8 +44,7 @@ for epoch in range(epochs):
                 weather = weather_full[0:1, :, x, y]
                 terrain = torch.tensor(terrain)
                 pred = net(terrain, weather)
-                # output[:, x, y] = result[0][0].item()
-
+                # output[:, x, y] = pred[0][0].item()
                 optimizer.zero_grad()
                 loss = criterion(pred[0].double(), target[:, x, y])
                 loss.backward()
@@ -55,4 +58,4 @@ for epoch in range(epochs):
 print('Finished Training')
 
 PATH = './firecast.pth'
-torch.save(net.state_dict(), PATH)
+torch.save(net, PATH)
