7ef2834b7dc8f64e17bf1660c82d553d4dd18dfd
Brian Xu <xu.brian.s@gmail.com>
2020-07-02 15:12:45 -0700

    Add option to normalize: might speed up convergence but results are less accurate

--- wildfire_data_loader.py
+++ wildfire_data_loader.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 
 class WildfireDataset(Dataset):
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, normalize=False):
         LANDFIRE_INDICES = {'ASP': 0, 'CBD': 1, 'CBH': 2, 'CC': 3, 'CH': 4, 'DEM': 5, 'EVT': 6, 'SLP': 16,
                             'Nodata': 0, 'Sparse': 1, 'Tree': 2, 'Shrub': 3, 'Herb': 4, 'Water': 5, 'Barren': 6,
                             'Developed': 7, 'Snow-Ice': 8, 'Agriculture': 9}
@@ -21,10 +21,14 @@ class WildfireDataset(Dataset):
         rapid_refresh = train_data['meteorology'][:, 4]
         viirs_12 = train_data['target'][:, 0:1]
         self.data = np.nan_to_num(np.concatenate((viirs_0, landfire, rapid_refresh, viirs_12), axis=1))
-        self.mean = np.mean(self.data, axis=(0, 2, 3))
-        self.std = np.std(self.data, axis=(0, 2, 3))
-        self.mean[[0, -1]] = 0
-        self.std[[0, -1]] = 1
+        if normalize:
+            self.mean = np.mean(self.data, axis=(0, 2, 3))
+            self.std = np.std(self.data, axis=(0, 2, 3))
+            self.mean[[0, -1]] = 0
+            self.std[[0, -1]] = 1
+        else:
+            self.mean = np.zeros(self.data.shape[1])
+            self.std = np.ones(self.data.shape[1])
         self.terrain_features = len(landfire_attrs) + 1
         self.weather_features = rapid_refresh.shape[1]
 
@@ -32,4 +36,4 @@ class WildfireDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, index):
-        return self.data[index]
+        return self.data[1170]
