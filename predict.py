4dc7d0330e1e59d08cea9d301efed25e3d8cd91e
Brian Xu <xu.brian.s@gmail.com>
2020-07-04 16:08:41 -0700

    First draft of prediction

---
+++ predict.py
@@ -0,0 +1,19 @@
+import torch
+
+
+class Predictor:
+
+    def __init__(self, firecast_path, terrain_path, weather_path, gpu: bool = False):
+        self.device = torch.device('cuda:0' if (torch.cuda.is_available() and gpu) else 'cpu')
+        self.net = torch.load(firecast_path, map_location=self.device)
+        self.net.eval()
+        self.terrain_norm = torch.load(terrain_path, map_location=self.device)
+        self.weather_norm = torch.load(weather_path, map_location=self.device)
+
+    def predict(self, terrain, weather) -> torch.tensor:
+        with torch.no_grad():
+            terrain = self.terrain_norm(torch.tensor(terrain))
+            weather = self.weather_norm(torch.tensor(weather))
+            pred = self.net(terrain, weather)
+
+        return pred.to('cpu').item()
