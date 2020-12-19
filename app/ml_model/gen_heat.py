68af1813ea28bb41ac774d3fa9146c410ae9487f
Brian Xu <xu.brian.s@gmail.com>
2020-12-18 21:19:19 -0800

    Changes made after actually running the app

--- app/ml_model/gen_heat.py
+++ app/ml_model/gen_heat.py
@@ -1,15 +1,16 @@
 from ml_model.data import DataCollector
 from ml_model.gen_predictions import MapPredictor
-from firecast import FireCast
+# noinspection PyUnresolvedReferences
+from firecast import FireCast  # necessary for when pytorch loads the model weights
 
 lance_key = open('lance_key.txt').read()
 dc = DataCollector(lance_key)
-mp = MapPredictor(15, dc.viirs_data(), dc.rapid_refresh_data(), 'maps/evc.tiff',
-                  'maps/slp.tiff', 'weights/firecast.pth')
+mp = MapPredictor(15, dc.viirs_data(), dc.rapid_refresh_data(),
+                  'maps/evc.tiff', 'maps/slp.tiff', 'weights/firecast.pth')
 points = []
 for p in mp.gen_predictions():
     if round(p[1], 2) > 0:
-        points.append([p[0].x, p[0].y, f'{p[1]*35:.4f}'])
+        points.append([p[0].x, p[0].y, f'{p[1]:.4f}'])
 
 with open("../static/latest.js", "w") as f:
     f.write(f'var firePoints = {points};')
--- train/train.py
+++ train/train.py
@@ -7,14 +7,15 @@ from firecast import FireCast
 from wildfire_data_loader import WildfireDataset
 
 DATASET_PATH = 'wildfire_data/uci_ml_hackathon_fire_dataset_2012-05-09_2013-01-01_10k_train_v2-002.hdf5'
-trainset = WildfireDataset(DATASET_PATH)
+training_set = WildfireDataset(DATASET_PATH)
 
-trainloader = torch.utils.data.DataLoader(trainset, shuffle=True, pin_memory=True)
+data_loader = torch.utils.data.DataLoader(training_set, shuffle=True, pin_memory=True)
 
 area = 15
 radius = (area - 1) // 2
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-net = FireCast(area, terrain_features=trainset.terrain_features, weather_features=trainset.weather_features).to(device)
+net = FireCast(area, terrain_features=training_set.terrain_features,
+               weather_features=training_set.weather_features).to(device)
 
 lr = 0.0001
 criterion = nn.BCELoss()
@@ -22,25 +23,25 @@ optimizer = optim.RMSprop(net.parameters(), lr=lr)
 epochs = 10
 
 for epoch in range(epochs):
-    for i, data in enumerate(trainloader):
+    for i, data in enumerate(data_loader):
         running_loss = 0.0
         data = data[0]
         target = data[-1]
         data = data[:-1]
         pad_width = ((0, 0), (radius, radius), (radius, radius))
         # terrain_full = torch.tensor(np.pad(data[:trainset.terrain_features], pad_width))
-        terrain_full = data[:trainset.terrain_features]
-        weather_full = data[trainset.terrain_features:]
+        terrain_full = data[:training_set.terrain_features]
+        weather_full = data[training_set.terrain_features:]
         # output = np.zeros(target.shape)
         size = 0
         for x in range(radius, target.shape[0] - radius):
             for y in range(radius, target.shape[1] - radius):
                 size += 1
-                terrain = np.zeros((1, trainset.terrain_features, area, area))
-                for layer in range(trainset.terrain_features):
+                terrain = np.zeros((1, training_set.terrain_features, area, area))
+                for layer in range(training_set.terrain_features):
                     terrain[0][layer] = terrain_full[layer, x - radius:x + radius + 1, y - radius:y + radius + 1]
                 if np.sum(terrain[0][0]) > 0:
-                    weather = weather_full[:, x, y].reshape((1, trainset.weather_features)).to(device)
+                    weather = weather_full[:, x, y].reshape((1, training_set.weather_features)).to(device)
                     terrain = torch.tensor(terrain).to(device)
                     pred = net(terrain, weather)
                     # output[x, y] = pred[0][0].item()
